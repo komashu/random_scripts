e7f7f37748e15c6c676ba25a05a89998031c4a85
shoe <shoe@steichen.local>
2023-03-30 23:25:54 -0500

    initial commit - script not working yet

---
+++ get_site_dependencies.py
@@ -0,0 +1,19 @@
+import json
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+caps = DesiredCapabilities.CHROME
+caps['goog:loggingPrefs'] = {'performance': 'ALL'}
+driver = webdriver.Chrome(desired_capabilities=caps)
+driver.get('https://stackoverflow.com/questions/52633697/selenium-python-how-to-capture-network-traffics-response')
+
+def process_browser_log_entry(entry):
+    response = json.loads(entry['message'])['message']
+    return response
+
+browser_log = driver.get_log('performance') 
+events = [process_browser_log_entry(entry) for entry in browser_log]
+events = [event for event in events if 'Network.response' in event['method']]
+
+for event in events:
+    event['params']['response']['url']
